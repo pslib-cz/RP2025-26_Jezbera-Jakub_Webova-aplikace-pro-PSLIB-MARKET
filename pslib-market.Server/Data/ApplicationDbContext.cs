4d9e3a6bae7f824e8007c55a1c4d40584a603434
Kubaejj <jakub.jezbera.023@pslib.cz>
2026-04-15 20:13:15 +0200

    uprava db

--- pslib-market.Server/Data/ApplicationDbContext.cs
+++ pslib-market.Server/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace pslib_market.Server.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Image> Images { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<BookReservation> BookReservations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
