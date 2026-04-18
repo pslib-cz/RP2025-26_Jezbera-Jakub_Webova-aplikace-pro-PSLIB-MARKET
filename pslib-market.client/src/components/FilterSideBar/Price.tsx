08463082e00bc2d0ba2bfb16aaf0fd6ae2564ff1
Kubaejj <jakub.jezbera.023@pslib.cz>
2026-04-18 20:13:31 +0200

    fix build error

--- pslib-market.client/src/components/FilterSideBar/Price.tsx
+++ pslib-market.client/src/components/FilterSideBar/Price.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 // import styles from './FilterSidebar.module.css'
 
 const Price = () => {
--- pslib-market.client/src/components/Layout.tsx
+++ pslib-market.client/src/components/Layout.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import { useAuth } from 'react-oidc-context';
 import styles from './Layout.module.css';
 import Header from './Header/Header';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    const auth = useAuth();
-    
     // if (auth.isLoading) return <div>Načítám...</div>;
     // if (!auth.isAuthenticated) {
     //     return (
